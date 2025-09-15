f3dbb980474712461f9b0ec1822a13a9dc2e5cd1
ryousuke kaga <k222ryousuke@gmail.com>
2025-09-16 00:30:52 +0900

    http server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/thecatblake/distributed-logging
 
 go 1.25.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"github.com/thecatblake/distributed-logging/internal/server"
-	"fmt"
+	"log"
 )
 
 func main() {
-	log := &server.Log{}
-	log.Append(server.Record{Value: []byte("Hello")})
-	log.Append(server.Record{Value: []byte("World")})
-	fmt.Println(log.Read(0))
-	fmt.Println(log.Read(1))
-	fmt.Println(log.Read(2))
+	srv := server.NewHTTPServer(":8000")
+	log.Fatal(srv.ListenAndServe())
 }
